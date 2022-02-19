a50dd640e1fc27aeb79ef119069af07496e4c53b
Egil <egc21@hotmail.com>
2022-02-19 16:15:27 -0500

    updates on Exercice 3.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'Mes tâches';
+  tabs: any = [{ label: "Composant A", link: "/a" }, { label: "Composant B", link: "/b" }]
 }
---
+++ src/app/components/b/b.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-b',
+  templateUrl: './b.component.html',
+  styleUrls: ['./b.component.css']
+  })
+export class BComponent implements OnInit {
+  name: string = "";
+  
+
+  constructor(private route: ActivatedRoute) { }
+
+
+
+ngOnInit(): void  { 
+     this.route.queryParams.subscribe(params => {
+     this.name = params['name'];
+
+     
+     
+   });
+ }
+}
